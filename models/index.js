08427f961ca382e6099661dc5734d102c492de9a
Hailey Flora <Haileycflora@gmail.com>
2023-04-21 00:43:16 -0400

    add association methods to models/index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE', //DO I NEED THIS?
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: '' //WHAT WOULD BE THE ALIAS HERE?
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: '' //WHAT WOULD BE THE ALIAS HERE?
+})
 
 module.exports = {
   Product,
